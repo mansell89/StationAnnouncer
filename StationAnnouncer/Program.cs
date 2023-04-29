98fd35f78a15836f0324842c5b3a14a674e3f852
mansell89 <mwellings89@gmail.com>
2023-04-29 16:41:39 +1000

    changed main code to use generateAnnouncement method of train class

--- StationAnnouncer/Program.cs
+++ StationAnnouncer/Program.cs
@@ -30,60 +30,7 @@ internal class Program
         //Split up csv into stations
         train.generateStopSequence(rawText);
         train.findExpressSections();
-        //get express sections
-        // generate description
-        // output description
-    
-        Console.WriteLine(generateAnnouncement(train));
+        Console.WriteLine(train.generateAnnouncement());
     }
-    private static string generateAnnouncement(Train train)
-    {
-        if (train.expressSections.Count == 0)
-        {
-            if (train.numberOfStops == 2)
-            {
-                return $"This train stops at {train.stopSequence[0].name} and {train.stopSequence[1].name} Only";
-            }
-            return "This train stops at all stations";
-        }
-        if (train.expressSections.Count == 1)
-        {
-            string returnString = "";
 
-            if (train.expressSections[0].stationList.Count == 1)
-            {
-                return $"This train stops at all stations except {train.expressSections[0].stationList[0].name}";
-            }
-
-            returnString = $"This train runs express from {train.expressSections[0].StartStation.name} to {train.expressSections[0].EndStation.name}";
-            if (train.expressSections[0].hasIntermediateStation)
-            {
-                returnString += $", stopping only at {train.expressSections[0].intermediateStation.name}";
-            }
-            return returnString;
-        }
-        if (train.expressSections.Count > 1)
-        {
-            string returnString = "This train";
-            for (int i = 0; i < train.expressSections.Count; i++)
-            {
-                if (i == 0)
-                {
-                    returnString += $" runs express from";
-                }
-                else
-                {
-                    returnString += $" then runs express from";
-                }
-                returnString += $" {train.expressSections[i].StartStation.name} to {train.expressSections[i].EndStation.name}";
-                if (train.expressSections[i].hasIntermediateStation)
-                {
-                    returnString += $", stopping only at {train.expressSections[i].intermediateStation.name}";
-                }
-
-            }
-            return returnString;
-        }
-        return "";
-    }
 }
